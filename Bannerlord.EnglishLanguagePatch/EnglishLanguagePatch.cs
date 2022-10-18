d954b936c3752b2de5f6c6d9237fcb332aa13d6e
ModerRAS <adqewsx@gmail.com>
2022-10-18 11:05:28 +0800

    Change `EnglishLanguagePatch` to `Transpiler`

--- Bannerlord.EnglishLanguagePatch/EnglishLanguagePatch.cs
+++ Bannerlord.EnglishLanguagePatch/EnglishLanguagePatch.cs
@@ -2,41 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml;
 using TaleWorlds.Localization;
 
 namespace Bannerlord.EnglishLanguagePatch {
     [HarmonyPatch(typeof(MBTextManager), "GetLocalizedText", new Type[] { typeof(string)})]
     public class EnglishLanguagePatch {
-        private static string RemoveComments(string localizedText) {
-            string text = "{%.+?}";
-            foreach (object obj in Regex.Matches(localizedText, text)) {
-                Match match = (Match)obj;
-                localizedText = localizedText.Replace(match.Value, "");
-            }
-            return localizedText;
-        }
-        static bool Prefix(ref string __result, string text) {
-            if (text != null && text.Length > 2 && text[0] == '{' && text[1] == '=') {
-                var i = 2;
-                for (; i < text.Length; i++) {
-                    if (text[i] != '}') {
-                        continue;
-                    } else {
-                        break;
-                    }
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+            var code = new List<CodeInstruction>(instructions);
+            int insertionIndex = -1;
+            for (int i = 0; i < code.Count - 1; i++) {
+                if (code[i].opcode == OpCodes.Ldstr && (string)code[i].operand == "English") {
+                    insertionIndex = i;
+                    code[i].operand = string.Empty;
                 }
-                var tmp = text.Substring(2, i - 2);
-                var text2 = LocalizedTextManager.GetTranslatedText(MBTextManager.ActiveLanguage, tmp);
-                if (string.IsNullOrEmpty(text2)) {
-                    text2 = text.Substring(i + 1);
-                }
-                __result = RemoveComments(text2);
             }
-            return false;
+
+
+            return code;
         }
-        
     }
 }
